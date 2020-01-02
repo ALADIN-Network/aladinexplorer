4c8f5a6ed3a8ae07ea1075b46aa92229dd2f0dd6
unknown <you@example.com>
2020-01-02 14:50:14 +0530

    Oracle rewards added

--- src/app/producer/producers/producers.component.ts
+++ src/app/producer/producers/producers.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { AlaService } from '../../services/ala.service';
-import { Observable, of, timer } from 'rxjs';
-import { map, share, switchMap } from 'rxjs/operators';
-import {environment} from '../../../environments/environment';
+import { Observable, of, timer, combineLatest } from 'rxjs';
+import { map, share, switchMap, catchError } from 'rxjs/operators';
+import { environment } from '../../../environments/environment';
 
 @Component({
   templateUrl: './producers.component.html',
@@ -16,7 +16,6 @@ export class ProducersComponent implements OnInit {
   chainStatus$: Observable<any>;
   oracles$: Observable<any>;
   oracleReward$: Observable<any>;
-  producers1$ : Observable<any>;
 
   constructor(
     private breakpointObserver: BreakpointObserver,
@@ -60,43 +59,81 @@ export class ProducersComponent implements OnInit {
     // );
 
 
-    this.producers$ = this.chainStatus$.pipe(
-      switchMap(chainStatus => this.alaService.getProducers().pipe(
-        map(producers => {
-          const votesToRemove = producers.reduce((acc, cur) => {
-            const percentageVotes = cur.total_votes / chainStatus.total_producer_vote_weight * 100;
-            if (percentageVotes * 200 < 100) {
-              acc += parseFloat(cur.total_votes);
-            }
-            return acc;
-          }, 0);
-          return producers.map((producer, index) => {
-            let position = parseInt(index) + 1;
-            let reward = 0;
-            let oracleR = 0;
-            let percentageVotes = producer.total_votes / chainStatus.total_producer_vote_weight * 100;
-            let percentageVotesRewarded = producer.total_votes / (chainStatus.total_producer_vote_weight - votesToRemove) * 100;
-           // console.log("producer: "+producer.owner+" producer bucket: "+producer.unpaid_blocks);
-           // console.log("perblock_bucket: "+chainStatus.perblock_bucket);
-          
-              reward += ((chainStatus.perblock_bucket*producer.unpaid_blocks)/chainStatus.total_unpaid_blocks)/10000;
-              console.log("before wpay: ",reward);
-              if (percentageVotesRewarded>=0.5){
-                reward += 164.3835616;
-              }
-              console.log("after wpay: ",reward);
-            return {
-              ...producer,
-              position: position,
-              reward: reward.toFixed(4),
-              votes: percentageVotes.toFixed(2),
-              numVotes:(producer.total_votes / 10000)
-            }
-           
-          });
-        }),
-      )),
-      share(),
+    // this.producers$ = this.chainStatus$.pipe(
+    //   switchMap(chainStatus => this.alaService.getProducers().pipe(
+    //     map(producers => {
+    //       const votesToRemove = producers.reduce((acc, cur) => {
+    //         const percentageVotes = cur.total_votes / chainStatus.total_producer_vote_weight * 100;
+    //         if (percentageVotes * 200 < 100) {
+    //           acc += parseFloat(cur.total_votes);
+    //         }
+    //         return acc;
+    //       }, 0);
+    //       return producers.map((producer, index) => {
+    //         let position = parseInt(index) + 1;
+    //         let reward = 0;
+    //         let oracleR = 0;
+    //         let percentageVotes = producer.total_votes / chainStatus.total_producer_vote_weight * 100;
+    //         let percentageVotesRewarded = producer.total_votes / (chainStatus.total_producer_vote_weight - votesToRemove) * 100;
+    //         reward += ((chainStatus.perblock_bucket * producer.unpaid_blocks) / chainStatus.total_unpaid_blocks) / 10000;
+    //         console.log("before wpay: ", reward);
+    //         if (percentageVotesRewarded >= 0.5) {
+    //           reward += 164.3835616;
+    //         }
+    //         console.log("after wpay: ", reward);
+    //         return {
+    //           ...producer,
+    //           position: position,
+    //           reward: reward.toFixed(4),
+    //           votes: percentageVotes.toFixed(2),
+    //           numVotes: (producer.total_votes / 10000)
+    //         }
+
+    //       });
+    //     }),
+    //   )),
+    //   share(),
+    // );
+
+    this.producers$ = combineLatest(
+      this.chainStatus$,
+      this.alaService.getProducers(),
+      this.alaService.getOraclesRewardTable(),
+      this.alaService.getOraclesTable()
+    ).pipe(
+      map(([chainStatus, producers, rewardTable, oracleTable]) => {
+        const votesToRemove = producers.reduce((acc, cur) => {
+          const percentageVotes = cur.total_votes / chainStatus.total_producer_vote_weight * 100;
+          if (percentageVotes * 200 < 100) {
+            acc += parseFloat(cur.total_votes);
+          }
+          return acc;
+        }, 0);
+        return producers.map((producer, index) => {
+          let position = parseInt(index) + 1;
+          let reward = 0;
+          let oracleR = 0;
+          oracleR = ((chainStatus.oracle_bucket * oracleTable.successful_requests) / rewardTable.total_successful_requests);
+          let percentageVotes = producer.total_votes / chainStatus.total_producer_vote_weight * 100;
+          let percentageVotesRewarded = producer.total_votes / (chainStatus.total_producer_vote_weight - votesToRemove) * 100;
+          reward += ((chainStatus.perblock_bucket * producer.unpaid_blocks) / chainStatus.total_unpaid_blocks) / 10000;
+          console.log("before wpay: ", reward);
+          if (percentageVotesRewarded >= 0.5) {
+            reward += 164.3835616;
+          }
+          console.log("after wpay: ", reward);
+          return {
+            ...producer,
+            position: position,
+            reward: reward.toFixed(4),
+            votes: percentageVotes.toFixed(2),
+            numVotes: (producer.total_votes / 10000),
+            oracleReward: oracleR
+          }
+
+        });
+      }),
+      share()
     );
   }
 
@@ -108,7 +145,7 @@ export class ProducersComponent implements OnInit {
     let timestamp_epoch: number = 946684800000;
     let dates_: number = (Date.now() / 1000) - (timestamp_epoch / 1000);
     let weight_: number = Math.floor(dates_ / (86400 * 7)) / 52;  //86400 = seconds per day 24*3600
-    console.log("vote weight:= "+Math.pow(2,weight_));
+    console.log("vote weight:= " + Math.pow(2, weight_));
     return Math.pow(2, weight_);
   }
 
@@ -122,5 +159,5 @@ export const PRODUCERS_COLUMNS = [
   'numVotes',
   'votes',
   'reward',
-  //'oracleReward'
+  'oracleReward'
 ];
